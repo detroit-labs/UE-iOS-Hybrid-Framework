94643d1a7e2aab3d8b6da824b9d30f011ebdfc97
Nate West <nwestr@gmail.com>
2017-09-01 11:32:00 -0400

    Add bundled image

--- Hybrid/HybridViewController.m
+++ Hybrid/HybridViewController.m
@@ -16,22 +16,12 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    NSBundle *bundle = [NSBundle bundleWithURL:[[NSBundle mainBundle] URLForResource:@"Hybrid" withExtension:@"bundle"]];
+    UIImage *globeImage = [UIImage imageNamed:@"icGlobe" inBundle:bundle compatibleWithTraitCollection:nil];
+    UIImageView *globeImageView = [[UIImageView alloc] initWithImage:globeImage];
+    globeImageView.contentMode = UIViewContentModeScaleAspectFit;
+    self.navigationItem.titleView = globeImageView;
 }
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
