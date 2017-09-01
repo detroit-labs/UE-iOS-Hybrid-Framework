b62f01e9c7d9087b5423ef5667b2f6b30f120032
Nate West <nwestr@gmail.com>
2017-09-01 11:10:05 -0400

    Add App target

---
+++ HybridApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HybridApp
+//
+//  Created by Nathaniel West on 9/1/17.
+//  Copyright © 2017 Detroit Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
